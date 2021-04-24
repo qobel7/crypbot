8f2c3b0fa603f8f66c1828d905cdb9c91e6a957f
caner.kavak <Qobel.3435>
2021-04-24 20:45:24 +0300

    tillson t3 eklendi

--- __init__.py
+++ __init__.py
@@ -1,7 +1,8 @@
 from Main import Main
+from sys import argv
 
 
 if __name__ == '__main__':
-    # Main().start()
+     Main().start(*argv[1:])
    # Main().binanceLocalIndicator()
-    Main().ftxLocalIndicator()
+   # Main().ftxLocalIndicator()
